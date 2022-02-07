e7850d8d14eb53d82ad1271105e7edd1775044a2
Jurij15 <jurij.grom1@gmail.com>
2022-02-07 21:10:42 +0100

    Add project files.

---
+++ CPUstressTest/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Stress test for CPU");
+Console.WriteLine("Press enter to start...");
+while (Console.ReadKey().Key == ConsoleKey.Enter) {
+    for (int ix = 0; ix < Environment.ProcessorCount; ++ix)
+    {
+        new Thread(loopForever).Start();
+    }
+    while (true)
+    {
+        Console.WriteLine("Stress testing");
+        while (true)
+        {
+            Console.WriteLine("Stress testing");
+            while (true)
+            {
+                Console.WriteLine("Stress testing");
+                while (true)
+                {
+                    Console.WriteLine("Stress testing");
+                }
+            }
+        }
+    }
+}
+
+static void loopForever()
+{
+    while (true) ;
+}
